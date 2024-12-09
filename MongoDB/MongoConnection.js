43a06f423d3a6663e8a4491523a2061a36dba7fa
tankvaishali <vaishalitank28603@gmail.com>
2024-12-09 15:54:07 +0530

    install mongodb and urlkey

--- MongoDB/MongoConnection.js
+++ MongoDB/MongoConnection.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 const MongoDB = async () => {
   await mongoose
     .connect(
-      "mongodb+srv://vaishalitankonly4loginsites:v1a2i3s4h5a6l7i8@blooddata.vpp2o.mongodb.net/solardb"
+      //   "mongodb+srv://vaishalitankonly4loginsites:v1a2i3s4h5a6l7i8@blooddata.vpp2o.mongodb.net/solardb"
+      "mongodb+srv://vaishalitankonly4loginsites:v1a2i3s4h5a6l7i8@blooddata.vpp2o.mongodb.net/solardb?retryWrites=true&w=majority"
     )
     .then(() => console.log("Connected!"));
 };
