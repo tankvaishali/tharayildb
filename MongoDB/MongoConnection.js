8c616e05be0b99781c102f90461382e4f61de590
tankvaishali <vaishalitank28603@gmail.com>
2024-12-09 17:12:07 +0530

    change  db name

--- MongoDB/MongoConnection.js
+++ MongoDB/MongoConnection.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const MongoDB = async () => {
   await mongoose
     .connect(
-        "mongodb+srv://vaishalitankonly4loginsites:v1a2i3s4h5a6l7i8@blooddata.vpp2o.mongodb.net/blood111"
+        "mongodb+srv://vaishalitankonly4loginsites:v1a2i3s4h5a6l7i8@blooddata.vpp2o.mongodb.net/SolarDb"
       // "mongodb+srv://vaishalitankonly4loginsites:v1a2i3s4h5a6l7i8@blooddata.vpp2o.mongodb.net/SolarDb?retryWrites=true&w=majority"
     )
     .then(() => console.log("Connected!"));
